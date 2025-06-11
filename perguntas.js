73a7127c4b95a642141ce745af5e418a185e1745
hfxpedro <pedro.ferrucio.silva@escola.pr.gov.br>
2025-06-11 08:48:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'nanatsu no taizai',
+    'Quem é o pecado da luxuria?',
+    'gowther.'
+)
+
+criaCartao(
+    'dragon ball',
+    'quem despertou o ss2 primeiro?',
+    'gohan.'
+)
+
+criaCartao(
+    'death note',
+    'Qual o nome do shinigame do kira?',
+    'ryuk.'
+)
+
+criaCartao(
+    'naruto',
+    'quem foi o primeiro uchiha a ter o rinnegan?',
+    'madara.'
+)
